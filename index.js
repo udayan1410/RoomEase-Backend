0b41cc24b357fe71a662d5f2726f13d79dde0ff9
udayan1410 <udayanbirajdar@gmail.com>
2020-09-30 15:14:28 -0700

    Created Models. Connected to DB

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var User = require('./models/User');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/', (req, res, next) => {
-    console.log(req.body);
+app.post('/', async (req, res, next) => {
+    let user = new User({ ...req.body });
+
+    try {
+        let s = await user.save();
+    } catch (err) {
+        console.log(err);
+    };
+
     res.send(req.body)
 })
 
-app.listen(8080, () => {
-    console.log("Server started on 8080");
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/RoomEase', { useUnifiedTopology: true, useNewUrlParser: true }, () => {
+    app.listen(8080, () => {
+        console.log("Server started on 8080");
+    })
 })
+
+
 
 
