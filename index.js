79875ea8aa043f724d64966f85fc5077f90246b8
udayan1410 <udayanbirajdar@gmail.com>
2020-09-30 00:28:36 -0700

    Created Room Model and User Model (#4)

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-//Sample Server file
-//Line by kewin
+var express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res, next) => {
+    console.log(req.body);
+    res.send(req.body)
+})
+
+app.listen(8080, () => {
+    console.log("Server started on 8080");
+})
+
+
