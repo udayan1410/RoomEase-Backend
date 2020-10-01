a84be9cc78e152f35bc2a9642377616e0b489902
udayan1410 <udayanbirajdar@gmail.com>
2020-09-30 18:01:49 -0700

    Signup done (#7)
    * Created Room Model and User Model
    
    * Created Models. Connected to DB
    
    * Created routes
    
    * Signup Route Done

--- routes/ProfileRoutes.js
+++ routes/ProfileRoutes.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
+
+router.get('/', (req, res, next) => {
+    res.send({
+        "name": "Udayan"
+    })
+})
+
+router.post('/', (req, res, next) => {
+    let user = new User({ ...req.body });
+    res.send(user)
+})
+
+
+module.exports = router;
 
 
 
