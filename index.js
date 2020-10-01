c5c354c2348313ffd41eeb87192a0509aea09552
udayan1410 <udayanbirajdar@gmail.com>
2020-09-30 17:18:32 -0700

    Created Routes (#6)
    * Created Room Model and User Model
    
    * Created Models. Connected to DB
    
    * Created routes

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var User = require('./models/User');
+
+var profileRoutes = require('./routes/ProfileRoutes');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/', async (req, res, next) => {
-    let user = new User({ ...req.body });
-
-    try {
-        let s = await user.save();
-    } catch (err) {
-        console.log(err);
-    };
-
-    res.send(req.body)
-})
-
-
+app.use('/profile', profileRoutes);
 
 mongoose.connect('mongodb://127.0.0.1:27017/RoomEase', { useUnifiedTopology: true, useNewUrlParser: true }, () => {
     app.listen(8080, () => {
